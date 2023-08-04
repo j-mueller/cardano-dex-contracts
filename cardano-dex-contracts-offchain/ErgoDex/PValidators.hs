43807348c2e4d3be474169fe0a28cc14b446b67b
Jann Müller <j-mueller@users.noreply.github.com>
2023-08-04 15:47:35 +0200

    Use cardano-api script types

--- cardano-dex-contracts-offchain/ErgoDex/PValidators.hs
+++ cardano-dex-contracts-offchain/ErgoDex/PValidators.hs
@@ -5,38 +5,39 @@ module ErgoDex.PValidators (
     redeemValidator,
 ) where
 
-import PlutusLedgerApi.Common (SerialisedScript)
-import           Codec.Serialise
-import qualified Data.ByteString.Lazy   as BSL
-import           Control.Monad.IO.Class
+import qualified Cardano.Api                          as C
+import           Cardano.Binary                       (DecoderError)
+import           Control.Monad.IO.Class               (MonadIO (..))
+import qualified Data.ByteString.Lazy                 as BSL
+import           Data.Proxy                           (Proxy (..))
 import           Paths_cardano_dex_contracts_offchain
 
 poolValidatorDataFileName :: String
 poolValidatorDataFileName = "pool.uplc"
 
-poolValidator :: (MonadIO m) => m SerialisedScript
+poolValidator :: (MonadIO m) => m (Either DecoderError (C.Script C.PlutusScriptV2))
 poolValidator = readValidatorFromFile poolValidatorDataFileName
 
 swapValidatorDataFileName :: String
 swapValidatorDataFileName = "swap.uplc"
 
-swapValidator :: (MonadIO m) => m SerialisedScript
+swapValidator :: (MonadIO m) => m (Either DecoderError (C.Script C.PlutusScriptV2))
 swapValidator = readValidatorFromFile swapValidatorDataFileName
 
 depositValidatorDataFileName :: String
 depositValidatorDataFileName = "deposit.uplc"
 
-depositValidator :: (MonadIO m) => m SerialisedScript
+depositValidator :: (MonadIO m) => m (Either DecoderError (C.Script C.PlutusScriptV2))
 depositValidator = readValidatorFromFile depositValidatorDataFileName
 
 redeemValidatorDataFileName :: String
 redeemValidatorDataFileName = "redeem.uplc"
 
-redeemValidator :: (MonadIO m) => m SerialisedScript
+redeemValidator :: (MonadIO m) => m (Either DecoderError (C.Script C.PlutusScriptV2))
 redeemValidator = readValidatorFromFile redeemValidatorDataFileName
 
-readValidatorFromFile :: (MonadIO m) => String -> m SerialisedScript
+readValidatorFromFile :: (MonadIO m) => String -> m (Either DecoderError (C.Script C.PlutusScriptV2))
 readValidatorFromFile dataFieldName = do
     path  <- liftIO $ getDataFileName dataFieldName
     bytes <- liftIO $ BSL.readFile path
-    pure $ deserialise bytes
+    pure $ C.deserialiseFromCBOR (C.proxyToAsType Proxy) (BSL.toStrict bytes)
