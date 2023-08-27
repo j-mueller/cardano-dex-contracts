8b9bbffd99b69f4d865c3463a8195e338e21f635
Jann Müller <j-mueller@users.noreply.github.com>
2023-08-27 15:23:23 +0100

    Use CardanoApi script constructor

--- cardano-dex-contracts-offchain/ErgoDex/CardanoApi.hs
+++ cardano-dex-contracts-offchain/ErgoDex/CardanoApi.hs
@@ -17,12 +17,8 @@ module ErgoDex.CardanoApi(
     plutarchScriptToCapiScript
 ) where
 
-import qualified Cardano.Api                                                as C
-import           Cardano.Binary                                             (DecoderError)
-import           Control.Monad                                              ((>=>))
+import qualified Cardano.Api.Shelley                                        as C
 import           Data.Bifunctor                                             (Bifunctor (..))
-import qualified Data.ByteString.Short                                      as BSS
-import           Data.Proxy                                                 (Proxy (..))
 import           Data.Text                                                  (Text)
 import qualified "cardano-dex-contracts-onchain" ErgoDex.PMintingValidators as Validators
 import qualified "cardano-dex-contracts-onchain" ErgoDex.PValidators        as Validators
@@ -33,29 +29,28 @@ import           PlutusLedgerApi.V1.Value                                   (Tok
 
 data CardanoApiScriptError =
     PlutarchScriptError Text
-    | CapiDeserialiseError DecoderError
     deriving Show
 
-poolScript :: Either CardanoApiScriptError (C.Script C.PlutusScriptV2)
+poolScript :: Either CardanoApiScriptError (C.PlutusScript C.PlutusScriptV2)
 poolScript = mkScript Validators.poolValidator
 
-depositScript :: Either CardanoApiScriptError (C.Script C.PlutusScriptV2)
+depositScript :: Either CardanoApiScriptError (C.PlutusScript C.PlutusScriptV2)
 depositScript = mkScript Validators.depositValidator
 
-swapScript :: Either CardanoApiScriptError (C.Script C.PlutusScriptV2)
+swapScript :: Either CardanoApiScriptError (C.PlutusScript C.PlutusScriptV2)
 swapScript = mkScript Validators.swapValidator
 
-redeemScript :: Either CardanoApiScriptError (C.Script C.PlutusScriptV2)
+redeemScript :: Either CardanoApiScriptError (C.PlutusScript C.PlutusScriptV2)
 redeemScript = mkScript Validators.redeemValidator
 
-poolNftMintingScript :: TxOutRef -> TokenName -> Either CardanoApiScriptError (C.Script C.PlutusScriptV2)
+poolNftMintingScript :: TxOutRef -> TokenName -> Either CardanoApiScriptError (C.PlutusScript C.PlutusScriptV2)
 poolNftMintingScript outRef = mkScript . Validators.poolNftMiningValidator outRef
 
-poolLqMintingScript :: TxOutRef -> TokenName -> Integer -> Either CardanoApiScriptError (C.Script C.PlutusScriptV2)
+poolLqMintingScript :: TxOutRef -> TokenName -> Integer -> Either CardanoApiScriptError (C.PlutusScript C.PlutusScriptV2)
 poolLqMintingScript outRef i = mkScript . Validators.poolLqMiningValidator outRef i
 
-plutarchScriptToCapiScript :: Script -> Either DecoderError (C.Script C.PlutusScriptV2)
-plutarchScriptToCapiScript (Script k) = C.deserialiseFromCBOR (C.proxyToAsType Proxy) $ BSS.fromShort $ serialiseUPLC k
+plutarchScriptToCapiScript :: Script -> C.PlutusScript C.PlutusScriptV2
+plutarchScriptToCapiScript (Script k) = C.PlutusScriptSerialised $ serialiseUPLC k
 
-mkScript :: Either Text Script -> Either CardanoApiScriptError (C.Script C.PlutusScriptV2)
-mkScript = first PlutarchScriptError >=> first CapiDeserialiseError . plutarchScriptToCapiScript
+mkScript :: Either Text Script -> Either CardanoApiScriptError (C.PlutusScript C.PlutusScriptV2)
+mkScript = fmap plutarchScriptToCapiScript . first PlutarchScriptError
